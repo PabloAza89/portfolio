782b32c1edb5125b350734277cd8f81143e3a14f
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2023-04-27 22:20:09 -0300

    developing dark mode

---
+++ client/src/components/DarkMode/DarkMode.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Box, Button,CardMedia, Typography } from '@mui/material';
+import { Link } from "react-router-dom";
+import ForwardIcon from '@mui/icons-material/Forward';
+import { useSelector, useDispatch } from 'react-redux';
+import BackButtonSX from '../../styles/BackButtonSX';
+import {
+  bgRed, flex, column, row, bgNone } from '../../styles/CommonsSX';
+
+function DarkMode() {
+
+  return (
+    <Link style={{ textDecoration: 'none' }} to="/portfolio">
+      <Button variant="contained"  sx={BackButtonSX().background}>
+        <ForwardIcon sx={BackButtonSX().icon}/>
+      </Button>
+    </Link>
+  )
+}
+
+export default DarkMode;
--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -63,7 +63,7 @@ function NavBar() {
         <Link style={NavBarSX().Projects()} to="/portfolio/Projects">{ english ? `Projects` : `Proyectos` }</Link>
         <Link style={NavBarSX().Certifications()} to="/portfolio/Certifications">{ english ? `Certifications` : `Certificaciones`}</Link>
         <Link style={NavBarSX().Contact()} to="/portfolio/Contact">{ english ? `Contact` : `Contacto` }</Link>
-        {/* <Button variant="contained" sx={{ position: wi() < '415' ? 'absolute' : 'null', top: wi() < '415' ? '2vh' : null, padding: '0px !important', minWidth: '2.1vw !important', 'max-width': '2.1vw !important', 'min-height': '2.1vw !important', 'max-height': '2.1vw !important'}}><WbSunnyIcon sx={{width: '1.6vw'}}/></Button> */}
+        <Button variant="contained" sx={{ position: wi() < '415' ? 'absolute' : 'null', top: wi() < '415' ? '2vh' : null, padding: '0px !important', minWidth: '2.1vw !important', 'max-width': '2.1vw !important', 'min-height': '2.1vw !important', 'max-height': '2.1vw !important'}}><WbSunnyIcon sx={{width: '1.6vw'}}/></Button>
       </ScrollContainer>
     </Box>
   )
