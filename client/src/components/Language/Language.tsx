1a5c211e4f213cec661ca3af04af873e75380609
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2023-05-11 19:24:37 -0300

    refactoring of Language and improving user view

--- client/src/components/Language/Language.tsx
+++ client/src/components/Language/Language.tsx
@@ -3,14 +3,12 @@ import { cyan } from '@mui/material/colors';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from "react-router-dom";
 import { languageChanger } from '../../actions';
-import lanEn from '../../images/lanEn.png';
-import lanEs from '../../images/lanEs.png';
-import { background, flagContourEn, lanEnFlag, flagContourEs, lanEsFlag } from '../../styles/LanguageSX';
+import { background, lanEnFlag, lanEsFlag } from '../../styles/LanguageSX';
 
 function Language() {
 
   const dispatch = useDispatch()
-  
+
   const english = useSelector((state: {english:boolean}) => state.english)
   const minPort = useSelector((state: {minPort:boolean}) => state.minPort)
   const minLand = useSelector((state: {minLand:boolean}) => state.minLand)
@@ -25,12 +23,8 @@ function Language() {
 
   return (
     <Box sx={background({ minPort, minLand, larPort, larLand, location:location.pathname, percentageResizedHeight })}>
-      <Box sx={flagContourEn({ english, minPort, minLand, medPort, medLand, larPort, maxStaticReference })}>
-        <CardMedia src={lanEn} onClick={() => dispatch(languageChanger(true))} sx={lanEnFlag({ minPort, minLand, medPort, medLand, larPort, maxStaticReference })}></CardMedia>
-      </Box>
-      <Box sx={flagContourEs({ english, minPort, minLand, medPort, medLand, larPort, maxStaticReference })} >
-        <CardMedia src={lanEs} onClick={() => dispatch(languageChanger(false))} sx={lanEsFlag({ minPort, minLand, medPort, medLand, larPort, maxStaticReference })}></CardMedia>
-      </Box>
+      <CardMedia component="div" onClick={() => dispatch(languageChanger(true))} sx={lanEnFlag({ english, minPort, minLand, medPort, medLand, larPort, maxStaticReference })}></CardMedia>
+      <CardMedia component="div" onClick={() => dispatch(languageChanger(false))} sx={lanEsFlag({ english, minPort, minLand, medPort, medLand, larPort, maxStaticReference })}></CardMedia>
     </Box>
   )
 }
--- client/src/styles/LanguageSX.ts
+++ client/src/styles/LanguageSX.ts
@@ -16,7 +16,6 @@ const background = ({ minPort, minLand, larPort, larLand, location, percentageRe
   return {
     ...asc, ...relative,
     background: 'red',
-    /* display: larLand && size.currentHeight < 300 ? 'none' : 'flex', */
     display: 'flex',
     pointerEvents:  larPort && percentageResizedHeight < 0.272 ? 'none' : larLand && percentageResizedHeight < 0.272 ? 'none' : 'null',
     transition: 'opacity .1s ease-in-out',
@@ -32,15 +31,13 @@ const background = ({ minPort, minLand, larPort, larLand, location, percentageRe
       },
     flexDirection: 'row',
     justifyContent: 'center',
-    //position: 'absolute',
-    //width: minPort ? '97vw' : minLand ? '97vw' : larPort ? '97vw' : '97vw',
-    width: '30vw' ,
+    width: minPort ? '40vw' : minLand ? '30vw' : '30vw',
     height: minPort ? '9vh' : minLand ? '16vh' : larPort ? '15vh' : '15vh',
     bottom: minPort ? '0vh' : '0.3vh'
   }
 }
 
-interface flagContourEnI {
+interface genI {
   english: boolean,
   minPort: boolean,
   minLand: boolean,
@@ -50,40 +47,18 @@ interface flagContourEnI {
   maxStaticReference: number
 }
 
-const flagContourEn = ({ english, minPort, minLand, medPort, medLand, larPort, maxStaticReference }: flagContourEnI) => {
+const lanEnFlag = ({ english, minPort, minLand, medPort, medLand, larPort, maxStaticReference }: genI) => {
   return {
-    position: 'relative',
-    marginRight: minPort ? '0.5vh' : minLand ? '0.5vw' : medPort ? `${maxStaticReference * 0.1}px` : medLand ? `${maxStaticReference * 0.1}px` : larPort ? `${maxStaticReference * 0.1}px` : `${maxStaticReference * 0.1}px`,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    backgroundColor: english ? cyan[100] : 'none',
-    width: minPort ? '14vw' : minLand ? '7vw' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.7}px` : `${maxStaticReference * 2.7}px`,
-    height: minPort ? '4.3vh' : minLand ? '9vh' : medPort ? `${maxStaticReference * 5.3}px` : medLand ? `${maxStaticReference * 5.3}px` : larPort ? `${maxStaticReference * 1.9}px` : `${maxStaticReference * 1.9}px`
-  }
-}
-
-interface lanEnFlagI {
-  minPort: boolean,
-  minLand: boolean,
-  medPort: boolean,
-  medLand: boolean,
-  larPort: boolean,
-  maxStaticReference: number
-}
-
-const lanEnFlag = ({ minPort, minLand, medPort, medLand, larPort, maxStaticReference }: lanEnFlagI) => {
-  return {
-    background: 'null',
     cursor: 'pointer',
     display: 'flex',
     flexDirection: 'row',
     alignSelf: 'center',
-    backgroundImage: `url(${lanEn})`,
-    width: minPort ? '20vw' : minLand ? '9vw' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.5}px` : `${maxStaticReference * 2.5}px`,
-    height: minPort ? '13vh' : minLand ? '9vh' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.6}px` : `${maxStaticReference * 2.6}px`,
-    backgroundSize: minPort ? '13vw 4.1vh' : minLand ? '6.5vw 8vh' : medPort ? `${maxStaticReference * 6.7}px` : medLand ? `${maxStaticReference * 6.7}px` : larPort ? `${maxStaticReference * 2.5}px ${maxStaticReference * 1.6}px` : `${maxStaticReference * 2.5}px ${maxStaticReference * 1.6}px`,
+    background: `url(${lanEn})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    border: english ? `solid ${cyan[100]} ${maxStaticReference * 0.4}px` : `solid transparent ${maxStaticReference * 0.4}px`,
+    width: minPort ? '15vw' : minLand ? '7.5vw' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.5}px` : `${maxStaticReference * 2.5}px`,
+    height: minPort ? '10vw' : minLand ? '5vw' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.6}px` : `${maxStaticReference * 2.6}px`,
     ':hover': {
       webkitFilter: 'brightness(.9)',
       'filter': 'brightness(.9)'
@@ -91,52 +66,23 @@ const lanEnFlag = ({ minPort, minLand, medPort, medLand, larPort, maxStaticRefer
   }
 }
 
-interface flagContourEsI {
-  english: boolean,
-  minPort: boolean,
-  minLand: boolean,
-  medPort: boolean,
-  medLand: boolean,
-  larPort: boolean,
-  maxStaticReference: number
-}
-
-const flagContourEs = ({ english, minPort, minLand, medPort, medLand, larPort, maxStaticReference }: flagContourEsI) => {
-  return {
-    position: 'relative',
-    /* marginRight: minPort ? '0.5vh' : minLand ? '0.5vw' : larPort ? '0.0vw' : '0vw', */
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    backgroundColor: english ? 'none' : cyan[100],
-    width: minPort ? '14vw' : minLand ? '7vw' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.7}px` : `${maxStaticReference * 2.7}px`,
-    height: minPort ? '4.3vh' : minLand ? '9vh' : medPort ? `${maxStaticReference * 5.3}px` : medLand ? `${maxStaticReference * 5.3}px` : larPort ? `${maxStaticReference * 1.9}px` : `${maxStaticReference * 1.9}px`
-  }
-}
-
-interface lanEsFlagI {
-  minPort: boolean,
-  minLand: boolean,
-  medPort: boolean,
-  medLand: boolean,
-  larPort: boolean,
-  maxStaticReference: number
-}
-
-const lanEsFlag = ({ minPort, minLand, medPort, medLand, larPort, maxStaticReference }: lanEsFlagI) => {
+const lanEsFlag = ({ english, minPort, minLand, medPort, medLand, larPort, maxStaticReference }: genI) => {
   return {
     cursor: 'pointer',
     display: 'flex',
     flexDirection: 'row',
     alignSelf: 'center',
-    backgroundColor: 'none',
-    backgroundImage: `url(${lanEs})`,
-    width: minPort ? '20vw' : minLand ? '9vw' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.5}px` : `${maxStaticReference * 2.5}px`,
-    height: minPort ? '13vh' : minLand ? '9vh' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.6}px` : `${maxStaticReference * 2.6}px`,
-    backgroundSize: minPort ? '13vw 4.1vh' : minLand ? '6.5vw 8vh' : medPort ? `${maxStaticReference * 6.7}px` : medLand ? `${maxStaticReference * 6.7}px` : larPort ? `${maxStaticReference * 2.5}px ${maxStaticReference * 1.6}px` : `${maxStaticReference * 2.5}px ${maxStaticReference * 1.6}px`,
-    ':hover': { webkitFilter: 'brightness(.9)', 'filter': 'brightness(.9)'}
+    background: `url(${lanEs})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    border: english ? `solid transparent ${maxStaticReference * 0.4}px` : `solid ${cyan[100]} ${maxStaticReference * 0.4}px`,
+    width: minPort ? '15vw' : minLand ? '7.5vw' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.5}px` : `${maxStaticReference * 2.5}px`,
+    height: minPort ? '10vw' : minLand ? '5vw' : medPort ? `${maxStaticReference * 7.3}px` : medLand ? `${maxStaticReference * 7.3}px` : larPort ? `${maxStaticReference * 2.6}px` : `${maxStaticReference * 2.6}px`,
+    ':hover': {
+      webkitFilter: 'brightness(.9)',
+      'filter': 'brightness(.9)'
+    }
   }
 }
 
-export { background, flagContourEn, lanEnFlag, flagContourEs, lanEsFlag }
+export { background, lanEnFlag, lanEsFlag }
