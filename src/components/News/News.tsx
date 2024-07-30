29f1a94aa9bc022d98362f504c95cf4d89577f08
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-07-30 14:04:44 -0300

    update News

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -12,19 +12,23 @@ function News() {
   const [ show, setShow ] = useState<boolean>(false)
 
   const preArray = [
-    { id: 12, date: '24-03-13', text: english ? ' Removed all JQuery code from Portfolio App  ' : ' Se removió todo el codigo JQuery de Portfolio App  ' },
-    { id: 11, date: '23-12-07', text: english ? ' Start using powerful Web Audio API  ' : ' Se empezó a utilizar la poderosa Web Audio API  ' },
-    { id: 10, date: '23-11-24', text: english ? ' Start developing new project: TicTacToe  ' : ' Se empezó a desarrollar un nuevo proyecto: TicTacToe  ' },
-    { id: 9, date: '23-10-31', text: english ? ' Start using .yml files for GitHub actions/workflow  ' : ' Se empezó a utilizar archivos .yml para las actions/workflow de GitHub  ' },
-    { id: 8, date: '23-09-09', text: english ? ' Using httpOnly cookies for secure login & data flow  ' : ' Usando cookies httpOnly para login y flujo de datos seguro  ' },
-    { id: 7, date: '23-08-30', text: english ? ' badWords searcher & highlighter algorithm done.  ' : ' Algorithmo buscador y resaltador de malas palabras listo.  ' },
-    { id: 6, date: '23-07-01', text: english ? ' Start using LocalStorage for data persistance  ' : ' Se empezó a utilizar LocalStorage para la persistencia de datos  ' },
-    { id: 5, date: '23-06-03', text: english ? ' Developing Skills Component (the most difficult)  ' : ' Desarrollando el Componente Habilidades (El más dificultoso)  ' },
-    { id: 4, date: '23-05-08', text: english ? ' Using own solution for use styles with variables (deprecated: performance leaks)  ' : ' Utilizando una solución propia para el uso de estilos con variables (obsoleto: mal performance)  ' },
-    { id: 3, date: '23-04-27', text: english ? ' Start using TS & screen size handlers  ' : ' Se empieza a usar TS & manejadores de tamaño de pantalla  ' },
-    { id: 2, date: '23-03-30', text: english ? ' Break for moving ;)  ' : ' Pausa por mudanza ;)  ' },
-    { id: 1, date: '23-02-21', text: english ? ' Break for vacations !  ' : ' Descanso de vacaciones !  ' },
-    { id: 0, date: '23-01-20', text: english ? ' Start developing my own first portfolio !  ' : ' Empezando a desarrollar mi primer portfolio propio !  ' },
+    { id: 16, date: '24-07-21', text: english ? ` Project ready to publish to Google Play  ` : ` Proyecto listo para publicar en Google Play  ` },
+    { id: 15, date: '24-06-26', text: english ? ` Using async-storage & r-n-bootsplash  ` : ` Uso de async-storage y r-n-bootsplash  ` },
+    { id: 14, date: '24-05-31', text: english ? ` Using React Native CLI: it's more flexible than Expo  ` : ` Usando React Native CLI: es más flexible que Expo  ` },
+    { id: 13, date: '24-04-15', text: english ? ` React Native Calculator project started  ` : ` Se inicia el proyecto React Native Calculator  ` },
+    { id: 12, date: '24-03-13', text: english ? ` Removed all JQuery code from Portfolio App  ` : ` Se removió todo el codigo JQuery de Portfolio App  ` },
+    { id: 11, date: '23-12-07', text: english ? ` Start using powerful Web Audio API  ` : ` Se empezó a utilizar la poderosa Web Audio API  ` },
+    { id: 10, date: '23-11-24', text: english ? ` Start developing new project: TicTacToe  ` : ` Se empezó a desarrollar un nuevo proyecto: TicTacToe  ` },
+    { id: 9, date: '23-10-31', text: english ? ` Start using .yml files for GitHub actions/workflow  ` : ` Se empezó a utilizar archivos .yml para las actions/workflow de GitHub  ` },
+    { id: 8, date: '23-09-09', text: english ? ` Using httpOnly cookies for secure login & data flow  ` : ` Usando cookies httpOnly para login y flujo de datos seguro  ` },
+    { id: 7, date: '23-08-30', text: english ? ` badWords searcher & highlighter algorithm done.  ` : ` Algorithmo buscador y resaltador de malas palabras listo.  ` },
+    { id: 6, date: '23-07-01', text: english ? ` Start using LocalStorage for data persistance  ` : ` Se empezó a utilizar LocalStorage para la persistencia de datos  ` },
+    { id: 5, date: '23-06-03', text: english ? ` Developing Skills Component (the most difficult)  ` : ` Desarrollando el Componente Habilidades (El más dificultoso)  ` },
+    { id: 4, date: '23-05-08', text: english ? ` Using own solution for use styles with variables (deprecated: performance leaks)  ` : ` Utilizando una solución propia para el uso de estilos con variables (obsoleto: mal performance)  ` },
+    { id: 3, date: '23-04-27', text: english ? ` Start using TS & screen size handlers  ` : ` Se empieza a usar TS & manejadores de tamaño de pantalla  ` },
+    { id: 2, date: '23-03-30', text: english ? ` Break for moving ;)  ` : ` Pausa por mudanza ;)  ` },
+    { id: 1, date: '23-02-21', text: english ? ` Break for vacations !  ` : ` Descanso de vacaciones !  ` },
+    { id: 0, date: '23-01-20', text: english ? ` Start developing my own first portfolio !  ` : ` Empezando a desarrollar mi primer portfolio propio !  ` },
   ]
 
   let [ startIndex, setStartIndex ]  = useState(0)
