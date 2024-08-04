b02a1c994ecea0205c5b5c6e1adebdd36c79230b
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-08-04 00:08:34 -0300

    developing image viewer

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,9 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
-import {/*  Button, */ SvgIcon } from '@mui/material';
-import { useSelector } from 'react-redux';
-import { Link } from "react-router-dom";
-import { HomeSvg } from '../../images/images';
-import Technologies from '../Technologies/Technologies';
+import React, { useEffect, useState, useLayoutEffect, useRef } from 'react';
 /* import ForwardIcon from '@mui/icons-material/Forward'; */
 import css from './ModalCSS.module.css';
 import ForwardIcon from '@mui/icons-material/Forward';
@@ -11,6 +6,7 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import CloseIcon from '@mui/icons-material/Close';
 import { Button } from '@mui/material';
+import { Diversity1Rounded } from '@mui/icons-material';
 
 function Modal({ images, imageIndex, setShowModal }: any) {
 
@@ -18,80 +14,115 @@ function Modal({ images, imageIndex, setShowModal }: any) {
 
 
   const [ currentZoom, setCurrentZoom ] = useState(1) // DEFAULT ZOOM
+  //let currentZoom = useRef(1) // DEFAULT ZOOM
+
+  //const refDiv = useRef(null);
+
 
   //console.log(currentZoom)
+  //console.log(currentZoom.current)
 
+  // useEffect(() => {
+  //   let imageSCROLLa = document.getElementById('imageContainer')
+  //   if (imageSCROLLa !== null) {
+  //     //console.log("PERCENTAGE SCROLLED", percentageScrolled.current)
+  //     console.dir(imageSCROLLa)
+  //   }
+  // }, [])
+  
+  //let el = document.getElementById('imageInContainer')
+
+  // useEffect(() => {
+  //   let err = document.getElementById('imageInContainer')
+  //   if (err !== null) {
+  //     console.log("rrr", err.scrollWidth)
+  //   }
+  // })
   const zoomIn = () => {
     
-    
+    //let el = document.getElementById('imageInContainer')
     setCurrentZoom((curr: any) => curr + 0.5)
 
     
-  }
-  const zoomOut = () => { if (currentZoom !== 1) setCurrentZoom((curr: any) => curr - 0.5) }
+      //el.style.transform = `scale(${currentZoom + 0.5})`;
+      //console.log("MAX SCROLL", el.scrollWidth - el.clientWidth)
+      //console.log("MAX SCROLL", el.scrollWidth)
+      //el.scrollTo(((el.scrollWidth - el.clientWidth) / 100) * percentageScrolled.current, 0)
+      
+      // .then(() => {
+      //   if (el !== null) {
+      //     el.style.transform = `scale(${currentZoom})`;
+      //   }
+      // })
+      // .then(() => {
+        
+      // })
+    
+    
 
-  let imageInContainerEl = document.getElementById('imageInContainer')
-  let imageSCROLL = document.getElementById('imageContainer')
+    //   let calc = Math.floor(((imageSCROLL.scrollWidth - imageSCROLL.clientWidth) / 100) * percentageScrolled.current)
+
+    //   console.log("NEXT SCROLL", calc)
+
+    //   imageSCROLL.scrollTo(calc, 0)
+
+    
+  }
+
+  const zoomOut = () => { if (currentZoom !== 1) {
+    setCurrentZoom((curr: any) => curr - 0.5)
+    
+    //let imageInContainerEl = document.getElementById('imageInContainer')!.style.transform = `scale(${currentZoom})`;
+    //  imageInContainerEl.style.transform = `scale(${currentZoom})`;
+
+
+
+
+  } }
+  //const zoomOut = () => { if (currentZoom.current !== 1) currentZoom.current = currentZoom.current - 0.5 }
+
+  //let imageInContainerEl = document.getElementById('imageInContainer')
+  //let imageSCROLL = document.getElementById('imageContainer')
 
   // let totalScroll = useRef(0)
   // let amountScroll = useRef(0)
   // let percentageScrolled = useRef(0)
   const [ totalScroll, setTotalScroll ]  = useState(0)
   const [ amountScroll, setAmountScroll ]  = useState(0)
-  const [ percentageScrolled, setPercentageScrolled ]  = useState(0)
+  //const [ percentageScrolled, setPercentageScrolled ]  = useState(0)
+  let percentageScrolled = useRef(0)
 
-  // console.log("AMOUNT SCROLL", amountScroll)
-  // console.log("TOTAL SCROLL", totalScroll)
+   //console.log("AMOUNT SCROLL", amountScroll)
+   //console.log("TOTAL SCROLL", totalScroll)
+   
   
   //let scrollToX = useRef(0)
   //let scrollToX = useRef(0)
 
+
+  // useEffect(() => {
+  //   console.log("PERCENTAGE", percentageScrolled.current)
+  // }, [percentageScrolled])
+
   useEffect(() => {
+    let imageSCROLL = document.getElementById('imageContainer')
     if (imageSCROLL !== null) {
-
-
-      imageSCROLL.addEventListener("scrollend", function(e: Event) {
-          //console.dir(event.target)
-        //console.log("TOTAL", e.target.scrollWidth - e.target.clientWidth)
-        //console.log("AMOUNT", e.target.scrollLeft)
-        //console.log("PERCENTAGE", (e.target.scrollLeft * 100) / (e.target.scrollWidth - e.target.clientWidth))
-
-        // totalScroll.current = e.target.scrollWidth - e.target.clientWidth
-        // amountScroll.current = e.target.scrollLeft
-        // percentageScrolled.current = (amountScroll.current * 100) / totalScroll.current
-
-        
-
+      imageSCROLL.addEventListener("scroll", function(e: Event) {
         if (e.target !== null) {
           let target = e.target as HTMLInputElement
-          setTotalScroll(target.scrollWidth - target.clientWidth)
-          setAmountScroll(target.scrollLeft)
-          console.log("EVENT FIRED !")
+          // setTotalScroll(target.scrollWidth - target.clientWidth)
+          // setAmountScroll(target.scrollLeft)
+          //setPercentageScrolled((amountScroll * 100) / totalScroll)
+          // console.dir(imageSCROLL)
+          console.log("SCROLLED AMOUT:", target.scrollLeft)
+          // console.log("SCROLLED PERCENTAGE:", (target.scrollLeft * 100) / (target.scrollWidth - target.clientWidth))
+          //setPercentageScrolled((target.scrollLeft * 100) / (target.scrollWidth - target.clientWidth))
+          percentageScrolled.current = (target.scrollLeft * 100) / (target.scrollWidth - target.clientWidth)
+          //console.log("PERCENTAGE", percentageScrolled.current)
         }
-        
-        ///setPercentageScrolled((amountScroll * 100) / totalScroll)
-
-        
-
-        //scrollToX.current = (totalScroll.current / 100) * percentageScrolled.current
-        //console.log("A VER", (totalScroll.current / 100) * percentageScrolled.current)
-        //console.log("PERCENTAGE", percentageScrolled.current)
-
-        // console.log("AMOUNT SCROLL", amountScroll.current)
-        // console.log("TOTAL SCROLL", totalScroll.current)
-
-        // imageSCROLL.scrollTo(scrollToX, 0)
-        //imageSCROLL.scrollTo(0, 0)
-
       })
-
-      //return () => imageSCROLL.remove()
-      
     }
-    
-    
-  //}, [amountScroll, imageSCROLL, totalScroll])
-  }, [imageSCROLL])
+  }, [])
 
   //console.log("CURRENT ZOOM", currentZoom)
 
@@ -99,6 +130,12 @@ function Modal({ images, imageIndex, setShowModal }: any) {
   // console.log("TOTAL SCROLL", totalScroll)
 
 
+
+  
+    let imageInContainerEl = document.getElementById('imageInContainer')
+    
+
+  let imageSCROLL = document.getElementById('imageContainer')
 
   useEffect(() => {
 
@@ -119,11 +156,147 @@ function Modal({ images, imageIndex, setShowModal }: any) {
   
 
     // }
+
+    //console.log("ZOOM CHANGED !!")
+
     
-      if (imageInContainerEl !== null) {
-        imageInContainerEl.style.transform = `scale(${currentZoom})`;
-        //imageInContainerEl.style.transform = `translate(50%, 50%);`
-      }
+
+    // if (imageInContainerEl !== null) {
+    //   //imageInContainerEl.style.transform = `scale(${currentZoom.current})`;
+      
+    //   imageInContainerEl.style.transform = `scale(${currentZoom})`;
+    //   //imageInContainerEl.style.transform = `scale(${asd.value})`;
+
+    //   //console.log("PERCENTAGE SCROLLED", percentageScrolled)
+    //   //console.log("imageSCROLL.scrollWidth", imageSCROLL.scrollWidth)
+    //   //console.log("imageSCROLL.clientWidth", imageSCROLL.clientWidth)
+      
+
+    // }
+
+    // let imageSCROLL = document.getElementById('imageContainer')
+
+    // let qq = document.getElementById('imageContainer')!.scrollWidth
+    // console.log("QQ", qq)
+
+    
+
+    // if (imageSCROLL !== null) {
+    //   //console.log("TESTTTTTTT", imageSCROLL.scrollWidth)
+    //   console.log("TOTAL TO SCROLL", imageSCROLL.scrollWidth - imageSCROLL.clientWidth)
+
+    //   //let calc = Math.floor(((imageSCROLL.scrollWidth - imageSCROLL.clientWidth) / 100) * percentageScrolled)
+    //   //let calc = Math.floor(((imageSCROLL.scrollWidth - imageSCROLL.clientWidth) / 100) * percentageScrolled.current)
+
+    //   //console.log("NEXT SCROLL", calc)
+
+    //   //imageSCROLL.scrollTo(calc, 0)
+
+    // }
+
+    // async function aa() {
+        
+    //   // await new Promise<void>((resolve) => {
+    //   //     //console.log(i);
+    //   //     setCurrentZoom((curr: any) => curr + 0.5)
+    //   //     resolve();
+    //   // });
+
+    //   await new Promise<void>((resolve) => {
+    //     //console.log(i);
+    //     if (imageInContainerEl !== null) {
+    //       setTimeout(() => { // SETTIMEOUT HELP TO EXECUTE FUNCTIONS IN ORDER !
+    //         imageInContainerEl.style.transform = `scale(${currentZoom})`;
+    //       }, 0)
+          
+    //     }
+    //     resolve();
+    //   });
+
+    //   // await new Promise<void>((resolve) => {
+    //   //   //console.log(i);
+    //   //   setTimeout(() => {}, 0)
+    //   //   resolve()
+    //   // });
+
+    //   await new Promise<void>((resolve) => {
+    //     //console.log(i);
+    //     if (refDiv.current !== null) {
+    //       console.log("refDiv.current", (refDiv.current as HTMLElement).scrollWidth)
+    //     }
+        
+    //     // let imageSCROLL2 = document.getElementById('imageContainer')
+    //     //   if (imageSCROLL2 !== null) {
+    //     //        // el.style.transform = `scale(${currentZoom + 0.5})`;
+    //     //        //console.log("TOTAL", Math.floor(((el.scrollWidth - el.clientWidth) / 100) * percentageScrolled.current))
+    //     //        console.log("TOTAL", imageSCROLL2.scrollWidth)
+    //     //   }
+    //     resolve();
+    //   });
+
+      // await new Promise<void>((resolve) => {
+      //   //console.log(i);
+      //   let ell = document.getElementById('imageInContainer')
+      //   if (ell !== null) {
+      //        // el.style.transform = `scale(${currentZoom + 0.5})`;
+      //        //console.log("TOTAL", Math.floor(((el.scrollWidth - el.clientWidth) / 100) * percentageScrolled.current))
+      //        console.log("TOTAL", ell.scrollWidth)
+      //   }
+      //   resolve();
+      // });
+      
+    // }
+    // aa()
+
+    
+      setTimeout(() => { // SETTIMEOUT HELP TO EXECUTE FUNCTIONS IN ORDER !
+        if (imageInContainerEl !== null) {
+         imageInContainerEl.style.transform = `scale(${currentZoom})`;
+        }
+      }, 0)
+      
+        // if (refDiv.current !== null) {
+        //   console.log("refDiv.current", (refDiv.current as HTMLElement).scrollWidth)
+        // }
+
+        if (imageSCROLL !== null) {
+          console.log("TOTAL TO SCROLL", imageSCROLL.scrollWidth - imageSCROLL.clientWidth)
+        }
+    
+    
+    
+      // if (imageInContainerEl !== null && imageSCROLL !== null) {
+      //   imageInContainerEl.style.transform = `scale(${currentZoom})`;
+        
+      //   //imageInContainerEl.style.transform = `translate(50%, 50%);`
+      //   imageSCROLL.scrollTo(((imageSCROLL.scrollWidth - imageSCROLL.clientWidth) / 100) * percentageScrolled.current, 0)
+      // }
+
+      // async function fetchMyAPI() {
+      //   if (imageInContainerEl !== null && imageSCROLL !== null) {
+      //     imageInContainerEl.style.transform = `scale(${currentZoom})`;
+          
+      //     //imageInContainerEl.style.transform = `translate(50%, 50%);`
+      //     return imageSCROLL
+      //   }
+      // }
+      // fetchMyAPI().then((imageSCROLL) => {
+      //   if (imageSCROLL !== undefined) {
+      //     imageSCROLL.scrollTo(((imageSCROLL.scrollWidth - imageSCROLL.clientWidth) / 100) * percentageScrolled.current, 0)
+      //   }
+        
+      // })
+
+      //console.log("NEW ZOOM")
+
+      //setPercentageScrolled((amountScroll * 100) / totalScroll)
+      //setPercentageScrolled((target.scrollLeft * 100) / (target.scrollWidth - target.clientWidth))
+
+      // if (imageSCROLL !== null) {
+      //           //imageSCROLL.scrollTo((totalScroll.current / 100) * percentageScrolled.current, 0)
+      //   //imageSCROLL.scrollTo((totalScroll / 100) * percentageScrolled, 0)
+      //  imageSCROLL.scrollTo(((imageSCROLL.scrollWidth - imageSCROLL.clientWidth) / 100) * percentageScrolled, 0)
+      // }
 
       // if (imageSCROLL !== null) {
       //   //totalScroll.current = imageSCROLL.scrollWidth - imageSCROLL.clientWidth
@@ -179,7 +352,10 @@ function Modal({ images, imageIndex, setShowModal }: any) {
 
 
   //}, [currentZoom, imageInContainerEl, imageSCROLL, percentageScrolled, totalScroll])
-  }, [amountScroll, currentZoom, imageInContainerEl, imageSCROLL, totalScroll])
+  //}, [currentZoom, imageInContainerEl, imageSCROLL, percentageScrolled])
+  //}, [currentZoom, percentageScrolled])
+  }, [currentZoom])
+  //})
 
   // useEffect(() => { // MOUSE GRAB & DRAG EFFECT ON MOUSE DEVICES
   //   const el = document.getElementById('imageContainer');
@@ -252,6 +428,9 @@ function Modal({ images, imageIndex, setShowModal }: any) {
         <div
           className={css.imageContainer}
           id={`imageContainer`}
+          //ref={refDiv}
+          //onScroll={() => console.log("SCROLL")}
+          //onScrollCapture={() => console.log("SCROLL")}
         >
 
           <img
@@ -281,9 +460,11 @@ function Modal({ images, imageIndex, setShowModal }: any) {
             if (currentIndex === 0) {
               setCurrentIndex(images.length - 1)
               setCurrentZoom(1)
+              //currentZoom.current = 1
             } else {
               setCurrentIndex((curr: any) => curr - 1)
               setCurrentZoom(1)
+              //currentZoom.current = 1
             }
           }}
         >
@@ -297,9 +478,11 @@ function Modal({ images, imageIndex, setShowModal }: any) {
             if (currentIndex === images.length - 1) {
               setCurrentIndex(0)
               setCurrentZoom(1)
+              //currentZoom.current = 1
             } else {
               setCurrentIndex((curr: any) => curr + 1)
               setCurrentZoom(1)
+              //currentZoom.current = 1
             }
           }}
         >
@@ -319,7 +502,7 @@ function Modal({ images, imageIndex, setShowModal }: any) {
           variant="contained"
           className={css.button}
           onClick={() => { zoomIn() }}
-          disabled={ currentZoom === 8 ? true : false }
+          disabled={ currentZoom === 8.0 ? true : false }
         >
           <AddIcon className={css.iconRight}/>
         </Button>
