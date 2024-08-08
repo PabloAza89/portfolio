44eeb507315c882269fe64b4870e4ef7e55bf3fe
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-08-08 20:35:30 -0300

    using canvas for image manipulation

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -10,46 +10,129 @@ function Modal({ images, imageIndex, setShowModal }: any) {
 
   const [ currentIndex, setCurrentIndex ] = useState(imageIndex)
 
-  const [ currentZoom, setCurrentZoom ] = useState(100) // DEFAULT ZOOM
+  const [ currentZoom, setCurrentZoom ] = useState(1) // DEFAULT ZOOM
 
 
   let refCanvas = useRef<HTMLCanvasElement>(null)
     
+  const [ amount, setAmount ] = useState(0)
 
   useEffect(() => {
-    //image.src = images[imageIndex]
-    console.log("imageIndex", currentIndex)
+    console.log("currentZoom", currentZoom)
+    //console.log("LAUNCHED")
     let image = new Image()
     image.src = images[currentIndex]
     image.onload = function() {
-      //if (!refCanvas.current) return
       if (refCanvas.current !== null) {
         let ref = refCanvas.current
         const ctx = ref.getContext("2d");
         console.log("image.naturalWidth", image.naturalWidth)
         console.log("image.naturalHeight", image.naturalHeight)
   
+        //ref.width = image.naturalWidth * currentZoom
+        //ref.width = (image.naturalWidth * currentZoom) * 1.5
+        //ref.height = image.naturalHeight * currentZoom * 1.5
+        //ref.width = 2000
+        //ref.height = image.naturalHeight * currentZoom
         ref.width = image.naturalWidth
         ref.height = image.naturalHeight
   
         if (ctx !== null) {
-  
-          ctx.drawImage(image, 0, 0, ref.width, ref.height);
-          ctx.imageSmoothingEnabled = false;
 
+          ctx.imageSmoothingEnabled = false;
+          //ctx.drawImage(image, 0, 0, ref.width, ref.height);
+          
+          console.log("currentZoom", currentZoom)
+
+          //drawImage(image,
+          //  sx, sy, sWidth, sHeight,
+          //  dx, dy, dWidth, dHeight)
+          // ctx.drawImage(image,
+          //   0, 0, ref.width, ref.height,
+          //   0, 0, ref.width * currentZoom, ref.height * currentZoom
+          // );
+          // width  1920 // 960 // 480
+          // height 1040 // 520 // 260
+          if (currentZoom === 1) {
+            ctx.drawImage(image,
+              0, 0, ref.width, ref.height,
+              0, 0, ref.width * currentZoom, ref.height * currentZoom
+            );
+          } else {
+            ctx.drawImage(image,
+              0, 0, ref.width, ref.height,
+              -960, -260, ref.width * currentZoom, ref.height * currentZoom
+              // LEFT CEN RIG
+              // -0 -480 -960
+              // 0, 0, ref.width, ref.height,
+              // amount *1, 0, ref.width * currentZoom, ref.height * currentZoom
+            );
+          }
+          
+          //ctx.translate(100, 100); 
+          //ctx.drawImage(image, 0, 0, 1920, 1040);
+          
+          //ctx.scale(currentZoom, currentZoom);
         }
+      }
+    }
+  }, [images, currentIndex, currentZoom, amount])
+
+  
+
+  let mouseDown = (e:any) => {
+    //console.log("MOUSE DOWN")
+    //console.log("MOUSE DOWN", e.clientX)
+    console.log("MOUSE DOWN", e.clientX)
+    setAmount(e.clientX)
+  }
+
+  let mouseUp = (e:any) => {
+    //console.log("MOUSE UP")
+    console.log("MOUSE UP", e.clientX)
+    //setAmount()
+    setAmount((curr: any) => e.clientX - curr)
+  }
+
+  let mouseMove = (e:any) => {
+    //console.log("MOUSE UP")
+    console.log("MOUSE MOVE", e.clientX)
+    setAmount(e.clientX)
+  }
+
+  console.log("AMOUNTTTT", amount)
+
+  // useEffect(() => {
+  //   console.log("currentZoom", currentZoom)
+  //   let image = new Image()
+  //   image.src = images[currentIndex]
+  //   image.onload = function() {
+  //     if (refCanvas.current !== null) {
+  //       let ref = refCanvas.current
+  //       const ctx = ref.getContext("2d");
+
+  //       if (ctx !== null) {
+  //         ctx.drawImage(image, 0, 0, ref.width, ref.height);
+  //         //ctx.scale(currentZoom, currentZoom);
+  //         ctx.scale(currentZoom, currentZoom);
+
+  //       }
     
         
-      }
-
-    }
-
-    
-  }, [images, currentIndex])
+  //     }
+  //   }
+  // }, [currentIndex, currentZoom, images, refCanvas])
   
 
   const zoomIn = () => {
+    //setCurrentZoom((curr: any) => curr + 50)
+    setCurrentZoom((curr: any) => curr + 0.5)
+      
+  }
 
+  const zoomOut = () => {
+    //setCurrentZoom((curr: any) => curr + 50)
+    setCurrentZoom((curr: any) => curr - 0.5)
       
   }
 
@@ -64,6 +147,11 @@ function Modal({ images, imageIndex, setShowModal }: any) {
           <canvas
             id={`canvasImage`}
             ref={refCanvas}
+            //onClick={(e) => test(e)}
+            className={css.canvasImage}
+            //onMouseDown={(e) => mouseDown(e)}
+            //onMouseUp={(e) => mouseUp(e)}
+            onMouseMove={(e) => mouseMove(e)}
           />
       </div>
       <div className={css.buttonsContainer}>
@@ -102,8 +190,8 @@ function Modal({ images, imageIndex, setShowModal }: any) {
         <Button
           variant="contained"
           className={css.button}
-          //onClick={() => { zoomOut() }}
-          disabled={ currentZoom === 100 ? true : false }
+          onClick={() => { zoomOut() }}
+          disabled={ currentZoom === 1 ? true : false }
         >
           <RemoveIcon className={css.iconRight}/>
         </Button>
@@ -112,7 +200,7 @@ function Modal({ images, imageIndex, setShowModal }: any) {
           variant="contained"
           className={css.button}
           onClick={() => { zoomIn() }}
-          disabled={ currentZoom === 800 ? true : false }
+          disabled={ currentZoom === 8 ? true : false }
         >
           <AddIcon className={css.iconRight}/>
         </Button>
@@ -125,7 +213,8 @@ function Modal({ images, imageIndex, setShowModal }: any) {
           <CloseIcon className={css.iconRight}/>
         </Button>
         <div>
-          {(currentZoom / 100).toFixed(1)} x
+          {/* {(currentZoom / 100).toFixed(1)} x */}
+          {currentZoom.toFixed(1)} x
         </div>
 
       </div>
