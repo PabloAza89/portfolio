d49dde2aeb9ba91f00e95cf30561ab60c8c5d976
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2024-08-15 21:27:04 -0300

    fixed imageViewer image reload when resize Projects component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -14,19 +14,6 @@ function Projects() {
   const english = useSelector((state: { english:boolean }) => state.english)
   const [scrollSpeed, setScrollSpeed] = useState<any>(30)
 
-  const [ heightDev, setHeightDev ] = useState<number>(window.innerHeight)
-  const [ widthDev, setWidthDev ] = useState<number>(window.innerWidth)
-
-  // useEffect(() => {
-  //   const autoHideOnResize: any = () => {
-  //     let { innerHeight, innerWidth } = window
-  //     setHeightDev(innerHeight);
-  //     setWidthDev(innerWidth);
-  //   }
-  //   window.addEventListener('resize', autoHideOnResize);
-  //   //return () => window.removeEventListener('resize', autoHideOnResize);
-  // },[])
-
   console.log("HHHHHHHHHHHHHHHHHHHHHHHHHHHH")
 
   function useHorizontalScroll() {
@@ -49,7 +36,7 @@ function Projects() {
     return elRef;
   }
 
-  useEffect(() => { // MOUSE GRAB & DRAG HORIZONTAL
+  useEffect(() => { // POINTER GRAB & DRAG EFFECT ON MOUSE DEVICES
     const scrollTarget = document.getElementById('scrollTarget');
     const elX = document.getElementById('sliderRollProjects');
     const elY = document.getElementById('sliderBoxYProjects');
@@ -60,20 +47,22 @@ function Projects() {
       scrollTarget !== null && elX !== null && elY !== null
       && children !== null && parent !== null
     ) {
-      const mouseEnterOnX = () => {
+      const mouseEnter = () => {
         console.log("ENTERRRRRRRR")
         if (children.clientWidth > parent.clientWidth || parent.clientHeight < 360) elX.style.cursor = 'grab'; // GRAB WHEN ENTER (MOUSEENTER)
-        //let pos = { left: 0, x: 0 };
-        let pos = { left: 0, x: 0, top: 0, y: 0 };
+        let pos = {
+          left: 0, x: 0, // X
+          top: 0, y: 0 // Y
+        };
 
         const mouseDownHandler = function (e: any) {
           elX.style.cursor = 'grabbing';
           elX.style.userSelect = 'none';
           pos = {
-            left: elX.scrollLeft,
-            x: e.clientX,
-            top: elY.scrollTop,
-            y: e.clientY,
+            left: elX.scrollLeft, // X
+            x: e.clientX,         // X
+            top: elY.scrollTop,   // Y
+            y: e.clientY,         // Y
           }
           if (children.clientWidth > parent.clientWidth || parent.clientHeight < 360) {
             elX.addEventListener('mousemove', mouseMoveHandler)
@@ -86,10 +75,10 @@ function Projects() {
         }
 
         const mouseMoveHandler = function (e: any) { // HOW MUCH MOUSE HAS MOVED
-          const dx = e.clientX - pos.x;
-          elX.scrollLeft = pos.left - dx;
-          const dy = e.clientY - pos.y;
-          elY.scrollTop = pos.top - dy;
+          const dx = e.clientX - pos.x;   // X
+          elX.scrollLeft = pos.left - dx; // X
+          const dy = e.clientY - pos.y;   // Y
+          elY.scrollTop = pos.top - dy;   // Y
         }
 
         const mouseUpHandler = function () {
@@ -107,11 +96,9 @@ function Projects() {
           elX.style.cursor = 'default'
         })
       }
-      scrollTarget.addEventListener("mouseenter", mouseEnterOnX)
-      return () => scrollTarget.removeEventListener("mouseenter", mouseEnterOnX)
+      scrollTarget.addEventListener("mouseenter", mouseEnter)
+      return () => scrollTarget.removeEventListener("mouseenter", mouseEnter)
     }
-
-    
   }, [])
 
   let [ projectChosen, setProjectChosen ] = useState(`All Projects`)
@@ -157,72 +144,19 @@ function Projects() {
 
   let linkBlocked = useRef(false)
 
-
-
-  // useEffect(() => { // MOUSE GRAB & DRAG VERTICAL
-  //   const elY = document.getElementById('sliderBoxYProjects');
-  //   const parent = document.querySelector('[class*="ProjectsCSS_background"]')
-
-  //   if (elY !== null) {
-  //     const mouseEnterOnY = () => {
-  //       console.log("ENTER VERTICAL")
-  //       let pos = { top: 0, y: 0 };
-
-  //       const mouseDownHandlerY = function (e: any) {
-  //         pos = {
-  //           top: elY.scrollTop,
-  //           y: e.clientY,
-  //         }
-
-  //         if (parent !== null) {
-  //           if (parent.clientHeight < 360) { // > 372 START OVERFLOW === 372 - 12 = 360
-  //             console.log("ENTRO ACAAAAAAAAAAAAAAA")
-  //             elY.addEventListener('mousemove', mouseMoveHandlerY)
-  //             elY.addEventListener('mouseup', mouseUpHandlerY)
-  //           } else {
-  //             elY.removeEventListener('mousemove', mouseMoveHandlerY);
-  //             elY.removeEventListener('mouseup', mouseUpHandlerY);
-  //           }
-  //         }
-  //       }
-
-  //       const mouseMoveHandlerY = function (e: any) { // HOW MUCH MOUSE HAS MOVED
-  //         const dy = e.clientY - pos.y;
-  //         elY.scrollTop = pos.top - dy;
-  //       }
-
-  //       const mouseUpHandlerY = function () {
-  //         elY.style.removeProperty('user-select')
-  //         elY.removeEventListener('mousemove', mouseMoveHandlerY)
-  //         elY.removeEventListener('mouseup', mouseUpHandlerY)
-  //       }
-
-  //       elY.addEventListener('mousedown', mouseDownHandlerY);
-  //       elY.addEventListener('mouseleave', function() {
-  //         elY.removeEventListener('mouseup', mouseUpHandlerY);
-  //         elY.removeEventListener('mousedown', mouseDownHandlerY)
-  //         elY.removeEventListener('mousemove', mouseMoveHandlerY);
-  //       })
-  //     }
-  //     elY.addEventListener("mouseenter", mouseEnterOnY)
-  //     return () => elY.removeEventListener("mouseenter", mouseEnterOnY)
-  //   }
-  // }, [])
-
   const timeoutProjects = useRef<ReturnType<typeof setTimeout>>();
   const autoHideProjects = () => {
     (document.getElementById(`buttonFlap`) as HTMLElement).click()
     clearTimeout(timeoutProjects.current)
   }
 
-  // useEffect(() => {
-  //   let el = document.getElementById(`boxLower`)
-  //   if (heightDev > 523) {
-  //     if (el !== null && el.classList.length === 2) {
-  //       (document.getElementById(`buttonFlap`) as HTMLElement).click()
-  //     }
-  //   }
-  // }, [heightDev])
+  useEffect(() => {
+    let el = document.getElementById(`boxLower`)
+    let buttonFlap = document.getElementById(`buttonFlap`)
+    const checkHeight = () => { if (window.innerHeight > 524 && el?.classList.length === 2) buttonFlap?.click() }
+    window.addEventListener("resize", checkHeight)
+    return () => window.removeEventListener("resize", checkHeight)
+  }, [])
 
   const handleClickFlap = () => {
     clearTimeout(timeoutProjects.current)
@@ -398,12 +332,12 @@ function Projects() {
       </div>
 
       {
-        /* showModal &&
+        showModal &&
         <Modal
           images={array.map(e => e.media).flat()}
           index={index}
           setShowModal={setShowModal}
-        /> */
+        />
       }
 
     </div>
